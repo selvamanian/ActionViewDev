8b1a6420740a81427cb0554993f06e42c345929c
Sunil Parekh <sunil.parekh@gmail.com>
2013-04-14 22:01:40 +0100

    Initial attribute configuration data

---
+++ common/data/tbl_attribute_meta.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 14, 2013 at 06:21 PM
+-- Server version: 5.5.25
+-- PHP Version: 5.4.4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `actionview`
+--
+
+--
+-- Dumping data for table `tbl_attribute_meta`
+--
+
+INSERT INTO `tbl_attribute_meta` (`id`, `name`) VALUES
+(1, 'Exclusive group'),
+(2, 'Non-exclusive group'),
+(3, 'Sub-group'),
+(4, 'Company attribute'),
+(5, 'Contact attribute');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
